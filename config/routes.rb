6db2f8d5529130bb03286d00133e1f8a8d27114b
Evgeniy <Vologzhanin2018@gmail.com>
2023-05-25 22:03:54 +0500

    add comment in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root "events#index"
 
-  resources :events
-  resources :users, only: [:show, :edit, :update]
+  resources :events do
+    resources :comments, only: %i[ create destroy ]
+  end
+
+  resources :users, only: %i[ show edit update ]
 end
