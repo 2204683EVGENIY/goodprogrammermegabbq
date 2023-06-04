072ae2c15cabdee670cd2d03d3a984131b65756c
Evgeniy <Vologzhanin2018@gmail.com>
2023-06-05 00:09:23 +0500

    add letter opener

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ gem "carrierwave"
 gem "cssbundling-rails"
 gem "devise"
 gem "devise-i18n"
-gem "font-awesome-rails"
 gem "jsbundling-rails"
 gem "puma", "~> 5.0"
 gem "rails", "~> 7.0.4", ">= 7.0.4.3"
@@ -19,6 +18,7 @@ gem "turbo-rails"
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 group :development, :test do
+  gem "letter_opener"
   gem "sqlite3", "~> 1.4"
   gem "web-console"
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,9 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded any time
+  # In the development environment your application"s code is reloaded any time
   # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  # since you don"t have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -61,4 +61,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
