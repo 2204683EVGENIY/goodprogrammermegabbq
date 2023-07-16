1cdd5796c6aca9ee3dd011068b1ba1ebb909fe06
Evgeniy <Vologzhanin2018@gmail.com>
2023-07-16 23:36:35 +0500

    env variables deploy with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :passenger_restart_with_touch, true
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/master.key"
+append :linked_files, "config/database.yml", "config/master.key", "current/.env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
@@ -45,3 +45,17 @@ set :default_env, { path: "/home/deploy/.nvm/versions/node/v16.9.0/bin/:$PATH" }
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+append :linked_files, ".env"
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app) do
+        unless test("[ -f .env ]")
+          upload! '.env', ".env"
+        end
+      end
+    end
+  end
+end
