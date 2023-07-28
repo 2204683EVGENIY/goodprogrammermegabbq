8cbb636ff1e2d714b1b73b0902bc4313b7a2b963
Evgeniy <Vologzhanin2018@gmail.com>
2023-07-28 12:33:16 +0500

    correction remarks with omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "email"
+  config.omniauth :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
   config.omniauth :vkontakte, ENV["VKONTAKTE_CLIENT_ID"], ENV["VKONTAKTE_CLIENT_SECRET"], scope: "email"
   # config.omniauth :github, ENV["GITHUB_CLIENT_ID_LOCAL"], ENV["GITHUB_CLIENT_SECRET_LOCAL"], scope: "email"
   # config.omniauth :vkontakte, ENV["VKONTAKTE_CLIENT_ID_LOCAL"], ENV["VKONTAKTE_CLIENT_SECRET_LOCAL"], scope: "email"
